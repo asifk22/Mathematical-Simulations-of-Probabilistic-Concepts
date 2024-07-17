ffbf6b1450abcd487d74a86ecff4c0f47af0e228
asifk22 <159617986+asifk22@users.noreply.github.com>
2024-07-17 21:22:18 +0530

    first commit

--- monty_hall_problem/3.py
+++ monty_hall_problem/3.py
@@ -66,3 +66,4 @@ if __name__ == "__main__":
         print('Ratio of probability for switching to sticking: {:.2f}'.format(probability_win_switch / probability_win_stick))
 
         plot_surface()
+#
